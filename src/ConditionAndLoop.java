674a2ff04d791f0d9152b31fcebbfb4aaf3a5ba8
Hajime <chen2346897@gmail.com>
2025-04-28 11:38:42 +0800

    test

--- src/ConditionAndLoop.java
+++ src/ConditionAndLoop.java
@@ -4,41 +4,41 @@ public class ConditionAndLoop {
          * 條件式
          * 
          * if (條件) {
-         *     條件成立時執行的程式碼
+         * 條件成立時執行的程式碼
          * } else {
-         *     條件不成立時執行的程式碼
+         * 條件不成立時執行的程式碼
          * }
          */
-        // int a = 2;
-        // int b = 3;
+        int a = 2;
+        int b = 3;
 
-        // if((a <= b)) {
-        //     System.out.println("a 的確 <= b");
-        // } else {
-        //     System.out.println("a 沒有 <= b");
-        // }
+        if (!(a <= b)) {
+            System.out.println("a 的確 <= b");
+        } else {
+            System.out.println("a 沒有 <= b");
+        }
 
         /*
          * else if
          * 
          * if (條件1) {
-         *    條件1成立時執行的程式碼
+         * 條件1成立時執行的程式碼
          * } else if (條件2) {
-         *    條件2成立時執行的程式碼
+         * 條件2成立時執行的程式碼
          * } else {
-         *    條件1跟條件2都不成立時執行的程式碼
+         * 條件1跟條件2都不成立時執行的程式碼
          * }
          */
         // int a = 2;
 
         // if(a == 2) {
-        //     System.out.println("a : 2");
+        // System.out.println("a : 2");
         // } else if(a == 3) {
-        //     System.out.println("a : 3");
+        // System.out.println("a : 3");
         // } else if(a == 4) {
-        //     System.out.println("a : 4");
+        // System.out.println("a : 4");
         // } else {
-        //     System.out.println("a 為2,3,4以外的數字");
+        // System.out.println("a 為2,3,4以外的數字");
         // }
 
         /*
@@ -48,29 +48,33 @@ public class ConditionAndLoop {
         // int a = 2;
         // int b = 3;
 
-        // String min = (a < b) ?  "A < B" :  "B > A";
+        // String min = (a < b) ? "A < B" : "B > A";
         // System.out.println(min);
+
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= 9; j++) {
+                System.out.println(i + "x" + j + "=" + i * j);
+            }
+        }
 
         /*
          * 53,54 這兩行 效果等於以下寫法
          */
         // if(a < b) {
-        //     System.out.println("A < B");
+        // System.out.println("A < B");
         // } else {
-        //     System.out.println("B > A");
+        // System.out.println("B > A");
         // }
 
-        
         /*
          * for 迴圈
          * for (初始值; 進入迴圈條件; 增量/減量) {
-         *     條件成立時執行的程式碼
+         * 條件成立時執行的程式碼
          * }
          */
         // for(int index = 0; index <= 10; index ++) {
-        //     System.out.println(index);
+        // System.out.println(index);
         // }
-
 
         /*
          * for 迴圈 將array所有元素印出來範例
@@ -80,9 +84,8 @@ public class ConditionAndLoop {
         // int[] bingo = {7, 22, 89, 55};
         // for(int index = 0; index < bingo.length; index ++) {
         // for(int index = 0; index <= bingo.length-1; index ++) {
-        //     System.out.println(bingo[index]);
+        // System.out.println(bingo[index]);
         // }
-
 
         /*
          * for 迴圈 減量範例
@@ -90,28 +93,26 @@ public class ConditionAndLoop {
          * 可用Debug 觀察 index 的變化
          */
         // for(int index = 10; index > 0; index --) {
-        //     System.out.println(index);
+        // System.out.println(index);
         // }
-
 
         /*
          * for each 迴圈
          * for (資料型態 單一變數名稱 : 陣列變數名稱) {
-         *     條件成立時執行的程式碼
+         * 條件成立時執行的程式碼
          * }
          */
         // String[] students = { "A1", "A2", "A3" };
         // for (String stu : students) {
-        //     System.out.println(stu);
+        // System.out.println(stu);
         // }
-
 
         /*
          * 105 ~ 108 這幾行 效果等於以下寫法
          */
         // String[] students = { "A1", "A2", "A3" };
         // for (int index = 0; index <= students.length - 1; index++) {
-        //     System.out.println(students[index]);
+        // System.out.println(students[index]);
         // }
 
     }
